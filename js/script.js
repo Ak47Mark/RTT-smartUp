763a2bd77d5018a551aa8c609db6411ce25b1bbd
Varga Márk <varga.mark@dolphio.hu>
2019-08-01 16:04:14 +0200

    Havi munkaidő fixálás + Havi munkaidő + Munkaidő vége, plusz idő jelzése

--- js/script.js
+++ js/script.js
@@ -6,12 +6,21 @@ $(document).ready(function () {
         console.log('Value currently is ' + back);
         workHour = result.key
     });
-    console.log(workHour);
     var date = new Date();
     var lastDay = new Date(date.getYear(), date.getMonth() + 1, 0);
     var firstDay = new Date(date.getYear(), date.getMonth(), 1);
     $('#tracking').append('<br><span id="backtime"><span>');
     $("#favorites").after('<div id="customTime" ng-controller="FavoritesCtrl" class="ng-scope" style="position: fixed; left: 0; top: 135px; background-color: white; min-width: 115px; min-height: 32px; border: 1px solid #ccc; margin-top: 3px; border-bottom-right-radius: 4px; padding: 5px; box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2); -moz-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2); -webkit-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);"><p style="font-weight: 600">Munkaórák</p><p id="customWeek">Heti: <span></span></p><p id="customMonth">Havi: <span></span></p></div>');
+
+    function monthWokdays(){
+        var days = 0;
+        for(var i = 1; i == date.getDate(); i++ ){
+            if(new Date(date.getYear(), date.getMonth(), i).getDay() < 6){
+                days++
+            }
+        }
+        return days;
+    }
 
     setInterval(function () {
         $('#customTime').css('top', 152 + $("#favorites").height());
@@ -19,19 +28,32 @@ $(document).ready(function () {
         weekH = Math.floor(week);
         weekM = (week % 1) * 60;
 
-        var month = Math.round((((Math.ceil((date.getDate() + firstDay.getDay() + 1) / 7) * 5) - firstDay.getDay() + 1) * workHour) * 100) / 100;
+        var month = monthWokdays() * workHour;
         monthH = Math.floor(month);
         monthM = (month % 1) * 60;
 
         $('#customWeek span').text(weekH + ":" + weekM);
         $('#customMonth span').text(monthH + ":" + monthM);
         var trackTime = $('#tracking')[0].textContent.split(' ')[9].split(':');
-        var backTime = (60 * workHour) - (parseInt(trackTime[0]) * 60 + parseInt(trackTime[1]));
-        var backTimeHour = Math.floor(backTime / 60);
-        var backTimeMinute = backTime % 60;
-        if (backTimeMinute < 10) {
-            backTimeMinute = "0" + backTimeMinute;
+        console.log(parseInt(trackTime[0])+(parseInt(trackTime[1])/60));
+        if(parseInt(trackTime[0])+(parseInt(trackTime[1])/60) < workHour) {
+            var backTime = (60 * workHour) - (parseInt(trackTime[0]) * 60 + parseInt(trackTime[1]));
+            var backTimeHour = Math.floor(backTime / 60);
+            var backTimeMinute = backTime % 60;
+            if (backTimeMinute < 10) {
+                backTimeMinute = "0" + backTimeMinute;
+            }
+            $('#backtime').text("Maradt: 0" + backTimeHour + ":" + backTimeMinute);
+        }else{
+            var backTime = (parseInt(trackTime[0]) * 60 + parseInt(trackTime[1])) - (60 * workHour);
+            var backTimeHour = Math.floor(backTime / 60);
+            var backTimeMinute = backTime % 60;
+            if (backTimeMinute < 10) {
+                backTimeMinute = "0" + backTimeMinute;
+            }
+            $('#backtime').text("Plusz: 0" + backTimeHour + ":" + backTimeMinute);
+            $('#backtime').css("color", "#62d462");
+
         }
-        $('#backtime').text("Maradt: 0" + backTimeHour + ":" + backTimeMinute);
     }, 1000);
 });
