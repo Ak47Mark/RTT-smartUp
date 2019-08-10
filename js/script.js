67036373f96f6968a234051b056d0df231de1fb9
Mark <ak47mark@gmail.com>
2019-08-10 16:33:47 +0200

    Havi munkaidő fixálás 2.0

--- js/script.js
+++ js/script.js
@@ -11,15 +11,18 @@ $(document).ready(function () {
     var lastDay = new Date(date.getYear(), date.getMonth() + 1, 0);
     var firstDay = new Date(date.getYear(), date.getMonth(), 1);
     $('#tracking').append('<br><span id="backtime"><span>');
-    $("#favorites").after('<div id="customTime" ng-controller="FavoritesCtrl" class="ng-scope" style="position: fixed; left: 0; top: 135px; background-color: white; min-width: 115px; min-height: 32px; border: 1px solid #ccc; margin-top: 3px; border-bottom-right-radius: 4px; padding: 5px; box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2); -moz-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2); -webkit-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);"><p style="font-weight: 600">Munkaórák</p><p id="customWeek">Heti: <span></span></p><p id="customMonth">Havi: <span></span></p></div>');
+    $("#favorites").after('<div id="customTime" ng-controller="FavoritesCtrl" class="ng-scope" style="position: fixed; left: 0; top: 135px; background-color: white; min-width: 115px; min-height: 32px; border: 1px solid #ccc; margin-top: 3px; border-bottom-right-radius: 4px; padding: 5px; box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2); -moz-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2); -webkit-box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);"><p style="font-weight: 600">Munkaórák</p><p>A mai nap végéig:</p><p id="customWeek">Heti: <span></span></p><p id="customMonth">Havi: <span></span></p></div>');
 
     function monthWokdays(){
         var days = 0;
-        for(var i = 0; i <= date.getDate(); i++ ){
-            if(parseInt(new Date(date.getFullYear(), date.getMonth() + 1, i).getDay()) < 5){
+        for(var i = 1; i <= date.getDate(); i++ ){
+            var day = parseInt(new Date(date.getFullYear(), date.getMonth(), i).getDay());
+            if(day < 6 && day > 0){
+                console.log(new Date(date.getFullYear(), date.getMonth(), i));
                 days++
             }
         }
+        console.log("Days:", days);
         return days;
     }
 
